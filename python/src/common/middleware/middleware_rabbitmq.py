eca11b5546a93037efb0759836a87692c9e6b469
undragonIII <jmgonzalezs@fi.uba.ar>
2026-04-09 15:43:09 -0300

    Pasan los test y camino feliz, empiezo a implementar las excepciones

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -8,30 +8,45 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
         self.host = host
         self.queue_name = queue_name
-
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(self.host))
-        self.channel = self.connection.channel()
-        self.channel.queue_declare(queue=self.queue_name)
+        try:
+            self.connection = pika.BlockingConnection(pika.ConnectionParameters(self.host))
+            self.channel = self.connection.channel()
+            self.channel.queue_declare(queue=self.queue_name)
+        except Exception as e:
+            pass
 
     def start_consuming(self, on_message_callback):
-        def callback(ch, method, properties, body):
-            ack = lambda:ch.basic_ack(delivery_tag = method.delivery_tag)
-            nack = lambda:ch.basic_nack(delivery_tag = method.delivery_tag)
-            on_message_callback(body, ack, nack)
-        self.channel.basic_consume(queue=self.queue_name, on_message_callback=callback)
-        self.channel.start_consuming()
 
+        try:
+
+            def callback(ch, method, properties, body):
+                ack = lambda:ch.basic_ack(delivery_tag = method.delivery_tag)
+                nack = lambda:ch.basic_nack(delivery_tag = method.delivery_tag)
+                on_message_callback(body, ack, nack)
+            self.channel.basic_consume(queue=self.queue_name, on_message_callback=callback)
+            self.channel.start_consuming()
+        except Exception as e:
+            pass
+        
     def stop_consuming(self):
-        self.channel.stop_consuming()
+        try:
+            self.channel.stop_consuming()
+        except Exception as e:
+            pass
 
     def send(self, message):
-        self.channel.basic_publish(exchange='',
-                              routing_key=self.queue_name,
-                              body=message)
+        try:
+            self.channel.basic_publish(exchange='',
+                                  routing_key=self.queue_name,
+                                  body=message)
+        except Exception as e:
+            pass
 
     def close(self):
-        self.connection.close()
-
+        try:
+            self.connection.close()
+        except Exception as e:
+            pass
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     def __init__(self, host, exchange_name, routing_keys):
         pass
