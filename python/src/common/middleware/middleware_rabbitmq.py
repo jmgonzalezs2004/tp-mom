98e75b8f381c31c87dafbf208fcfb04f8b5f6c27
undragonIII <jmgonzalezs@fi.uba.ar>
2026-04-09 16:26:54 -0300

    Incio definiciones de exchange basandome en las de queue

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -67,19 +67,63 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
             self.close()
         except Exception:
            pass 
+
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     def __init__(self, host, exchange_name, routing_keys):
-        pass
+        self.host = host
+        self.exchange = exchange_name
+        self.routing = routing_keys
+        try:
+            self.connection = pika.BlockingConnection(pika.ConnectionParameters(self.host))
+            self.channel = self.connection.channel()
+
+            # De manera predeterminada usa Exchange Directo, tambien existe topic, headers y fanout.
+            # Voy a basarme en el caso mas sencillo y usar fanout.
+            self.channel.exchange_declare(exchange_name = self.exchange_name,exchange_type='fanout')
+        except AMQPError as e:
+            raise MessageMiddlewareDisconnectedError()
 
     def start_consuming(self, on_message_callback):
-        pass
+        try:
+            def callback(ch, method, properties, body):
+                ack = lambda:ch.basic_ack(delivery_tag = method.delivery_tag)
+                nack = lambda:ch.basic_nack(delivery_tag = method.delivery_tag)
+                on_message_callback(body, ack, nack)
+
+            self.channel.basic_consume(queue=self.queue_name, on_message_callback=callback)
+            self.channel.start_consuming()
+        except AMQPError as e:
+            raise MessageMiddlewareDisconnectedError()
+        except Exception as e:
+            raise MessageMiddlewareMessageError()
 
     def stop_consuming(self):
-        pass
+        try:
+            self.channel.stop_consuming()
+        except AMQPError as e:
+            raise MessageMiddlewareDisconnectedError()
 
     def send(self, message):
-        pass
+        try:
+            self.channel.basic_publish(exchange=self.exchange,
+                                  routing_key=self.routing,
+                                  body=message)
+        except AMQPError as e:
+            raise MessageMiddlewareDisconnectedError()
+        except Exception as e:
+            raise MessageMiddlewareMessageError()
 
     def close(self):
-        pass
+        try:
+            self.connection.close()
+        except AMQPError as e:
+            raise MessageMiddlewareCloseError()
 
+
+    ## No lo pedia, pero me parecio buena idea agregar close al destructor
+    ## para asegurarse de flushear los mensajes y no tener comportamiento inesperado
+    def __del__(self):
+        try:
+            self.close()
+        except Exception:
+           pass 
