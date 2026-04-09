a43da62dbd903eaa3228b7b730e0e0bdef2d30f3
undragonIII <jmgonzalezs@fi.uba.ar>
2026-04-09 15:54:02 -0300

    Manejo de Exceptions basico

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -2,6 +2,14 @@ import pika
 import random
 import string
 from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
+from .middleware import (
+    MessageMiddlewareCloseError,
+    MessageMiddlewareDeleteError,
+    MessageMiddlewareDisconnectedError,
+    MessageMiddlewareMessageError
+)
+
+from pika.exceptions import AMQPConnectionError, AMQPError
 
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
     def __init__(self, host, queue_name):
@@ -12,41 +20,45 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
             self.connection = pika.BlockingConnection(pika.ConnectionParameters(self.host))
             self.channel = self.connection.channel()
             self.channel.queue_declare(queue=self.queue_name)
-        except Exception as e:
-            pass
+        except AMQPError as e:
+            raise MessageMiddlewareDisconnectedError()
 
     def start_consuming(self, on_message_callback):
 
         try:
-
             def callback(ch, method, properties, body):
                 ack = lambda:ch.basic_ack(delivery_tag = method.delivery_tag)
                 nack = lambda:ch.basic_nack(delivery_tag = method.delivery_tag)
                 on_message_callback(body, ack, nack)
             self.channel.basic_consume(queue=self.queue_name, on_message_callback=callback)
             self.channel.start_consuming()
+        except AMQPError as e:
+            raise MessageMiddlewareDisconnectedError()
         except Exception as e:
-            pass
-        
+            raise MessageMiddlewareMessageError()
+
     def stop_consuming(self):
         try:
             self.channel.stop_consuming()
-        except Exception as e:
-            pass
+        except AMQPError as e:
+            raise MessageMiddlewareDisconnectedError()
 
     def send(self, message):
         try:
             self.channel.basic_publish(exchange='',
                                   routing_key=self.queue_name,
                                   body=message)
+        except AMQPError as e:
+            raise MessageMiddlewareDisconnectedError()
         except Exception as e:
-            pass
+            raise MessageMiddlewareMessageError()
 
     def close(self):
         try:
             self.connection.close()
-        except Exception as e:
-            pass
+        except AMQPError as e:
+            raise MessageMiddlewareCloseError()
+            
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     def __init__(self, host, exchange_name, routing_keys):
         pass
