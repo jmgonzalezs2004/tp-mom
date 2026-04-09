9bee18383fafc32136f6c8682b8fcfb9f3c9038f
undragonIII <jmgonzalezs@fi.uba.ar>
2026-04-09 15:58:00 -0300

    Agrego destructor para asegurarse del flush

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -24,12 +24,12 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
             raise MessageMiddlewareDisconnectedError()
 
     def start_consuming(self, on_message_callback):
-
         try:
             def callback(ch, method, properties, body):
                 ack = lambda:ch.basic_ack(delivery_tag = method.delivery_tag)
                 nack = lambda:ch.basic_nack(delivery_tag = method.delivery_tag)
                 on_message_callback(body, ack, nack)
+
             self.channel.basic_consume(queue=self.queue_name, on_message_callback=callback)
             self.channel.start_consuming()
         except AMQPError as e:
@@ -58,7 +58,15 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
             self.connection.close()
         except AMQPError as e:
             raise MessageMiddlewareCloseError()
-            
+
+
+    ## No lo pedia, pero me parecio buena idea agregar close al destructor
+    ## para asegurarse de flushear los mensajes y no tener comportamiento inesperado
+    def __del__(self):
+        try:
+            self.close()
+        except Exception:
+           pass 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     def __init__(self, host, exchange_name, routing_keys):
         pass
